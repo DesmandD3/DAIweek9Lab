a7be67fa94032f0bb1ab130d46346357dcefc1f0
DesmandD3 <dking68@my.bcit.ca>
2022-11-02 15:16:56 -0700

    fixed vercel

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyByBzOwPf2k8AS7blm9yut3Z3_p5hvNUoQ",
-    authDomain: "week-9-lab-firebase.firebaseapp.com",
-    projectId: "week-9-lab-firebase",
-    storageBucket: "week-9-lab-firebase.appspot.com",
-    messagingSenderId: "989771624564",
-    appId: "1:989771624564:web:568301dc3d55a3e2cee121"
+    apiKey: process.env.NEXT_PUBLIC_apiKey,
+    authDomain: process.env.NEXT_PUBLIC_authDomain,
+    projectId: process.env.NEXT_PUBLIC_projectId,
+    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+    appId: process.env.NEXT_PUBLIC_appId
 };
 
 const app = initializeApp(firebaseConfig);
